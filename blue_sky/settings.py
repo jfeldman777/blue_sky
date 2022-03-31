b8c3df7a04548d2755afb6c3c6c65a0ca8c72b04
jfeldman777 <jfeldman777@gmail.com>
2022-03-31 23:08:17 +0300

    4

--- blue_sky/settings.py
+++ blue_sky/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT= Path(__file__).resolve().parent
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
